5d92138a5017d579f2dcce2bbd776a0b5bebbcb1
Philipp66904 <62964107+Philipp66904@users.noreply.github.com>
2024-04-10 15:18:54 +0200

    initial version

---
+++ common_package_py/common_node.py
@@ -0,0 +1,60 @@
+import rclpy
+from rclpy.node import Node
+
+from interfaces.msg import Heartbeat
+
+
+class CommonNode(Node):
+    """A class representing a common node.
+    
+        This class inherits from rclpy.node.Node and provides functionality for creating a node that sends heartbeat messages.
+    """
+
+    def __init__(self, id: str):
+        """Constructor to create a new node.
+
+        Args:
+            id (str): Node id
+        """
+        
+        super().__init__(id)
+        
+        # Unique name of the node
+        self.__id__ = id
+        
+        # Indicating if node is currently active and sending commands to interface node
+        self._active_ = False
+        
+        # Create a publisher for the "heartbeat" topic
+        self.__publisher__ = self.create_publisher(Heartbeat, 'heartbeat', 10)
+        
+        # Create a timer that sends a heartbeat message every 0.5s
+        self.__timer__ = self.create_timer(0.5, self.__timer_callback__)
+        
+        # Tick counting upwards with every heartbeat
+        self.__tick__ = 0
+
+    def __timer_callback__(self):
+        """
+        This method is called by the timer to publish a heartbeat message.
+
+        It creates a Heartbeat message, sets the sender ID, active status, and tick count,
+        and publishes the message using the publisher.
+        """
+        
+        msg = Heartbeat()
+        msg.sender_id = self.__id__
+        msg.active = self._active_
+        self.__tick__ += 1
+        msg.tick = self.__tick__
+        self.__publisher__.publish(msg)
+    
+    @property
+    def id(self) -> str:
+        """
+        Get the ID of the common node.
+
+        :return: The ID of the common node as string.
+        """
+        
+        return self.__id__
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'common_package_py'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='philipp',
+    maintainer_email='phg63865@thi.de',
+    description='Common package for all Python nodes',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'common_node = common_package_py.common_node:main',
+        ],
+    },
+)
