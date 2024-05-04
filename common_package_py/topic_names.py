9c7ee9e1bd5baf0cd864c5f571062a48d344c73a
Philipp66904 <philippg.pgb@gmail.com>
2024-05-04 22:35:29 +0200

    add global topic names

---
+++ common_package_py/topic_names.py
@@ -0,0 +1,21 @@
+class TopicNames:
+    # FCC Bridge
+    BatteryState = "uav_battery_state"
+    GPSPosition = "uav_gps_position"
+    MissionProgress = "uav_mission_progress"
+    Pose = "uav_pose"
+    RCState = "uav_rc_state"
+    SafetyLimits = "uav_safety_limits"
+    UAVCommand = "uav_command"
+    UAVHealth = "uav_health"
+    UAVWaypointCommand = "uav_waypoint_command"
+
+    # Mission Control
+    Control = "control"
+    Heartbeat = "heartbeat"
+    JobFinished = "job_finished"
+    MissionFinished = "mission_finished"
+    MissionStart = "mission_start"
+
+    # Marker Detection
+    QRCodeInfo = "qr_code_info"
